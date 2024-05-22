c34928dea10b9196e92ea0e0f2ebd9c62f86d6c8
Ased Mammad <mammad.ased@gmail.com>
2024-05-22 12:47:38 +0330

    🎨 Format source files

--- esp-now-serial-chat/esp-now-serial-chat.ino
+++ esp-now-serial-chat/esp-now-serial-chat.ino
@@ -4,17 +4,17 @@
 // Define SENDER or RECEIVER to select which device this program will run on
 #define SENDER
 #define MAX_TRANSMISSION_COUNT 5 // Max number of retransmissions before giving up
-#define MAX_MESSAGE_LENGTH 250 // Max supporte ESP-NOW message size is 250 bytes
+#define MAX_MESSAGE_LENGTH 250   // Max supporte ESP-NOW message size is 250 bytes
 
 #ifndef SENDER
-    #define RECEIVER
+#define RECEIVER
 #endif
 
 // TODO: Change this to your own MAC addresses
 #ifdef SENDER
-    uint8_t broadcastAddress[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
+uint8_t broadcastAddress[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
 #else
-    uint8_t broadcastAddress[] = {0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C};
+uint8_t broadcastAddress[] = {0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C};
 #endif
 
 // Define variables to store message to be sent
@@ -32,22 +32,26 @@ short transmissionCount = 0;
  * @param uint8_t sendStatus
  * @return void
  */
-void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus)
+{
   // Serial.print("Last Packet Send Status: ");
-  if (sendStatus == 0){
+  if (sendStatus == 0)
+  {
     Serial.println("Message was successfully sent.");
     char macStr[18];
     snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+             mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
     Serial.print("Sent to MAC: ");
     Serial.println(macStr);
   }
-  else if (transmissionCount < MAX_TRANSMISSION_COUNT){
+  else if (transmissionCount < MAX_TRANSMISSION_COUNT)
+  {
     Serial.println("Retransmitting the message...");
     transmissionCount++;
     sendMessage(messageToSend);
   }
-  else{
+  else
+  {
     Serial.println("Delivery failed, could not send the message.");
   }
 }
@@ -60,7 +64,8 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
  * @param uint8_t len
  * @return void
  */
-void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
+void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len)
+{
   // Serial.print("Bytes received: ");
   // Serial.println(len);
 
@@ -77,15 +82,17 @@ void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
  * @param String message to send
  * @return void
  */
-void sendMessage(String message){
-  uint8_t *buffer = (uint8_t*) message.c_str();
+void sendMessage(String message)
+{
+  uint8_t *buffer = (uint8_t *)message.c_str();
   size_t sizeBuff = sizeof(buffer) * message.length();
 
   // Send message via ESP-NOW
   esp_now_send(broadcastAddress, buffer, sizeBuff);
 }
- 
-void setup() {
+
+void setup()
+{
   // Init Serial Monitor
   Serial.begin(115200);
   Serial.setTimeout(50);
@@ -95,7 +102,8 @@ void setup() {
   WiFi.disconnect();
 
   // Init ESP-NOW
-  if (esp_now_init() != 0) {
+  if (esp_now_init() != 0)
+  {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
@@ -106,21 +114,22 @@ void setup() {
   // Once ESPNow is successfully Init, we will register for Send CB to
   // get the status of Trasnmitted packet
   esp_now_register_send_cb(OnDataSent);
-  
+
   // Register peer
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
-  
+
   // Register for a callback function that will be called when data is received
   esp_now_register_recv_cb(OnDataRecv);
 }
- 
-void loop() {
 
-  if (Serial.available() > 0) {
-    messageToSend = Serial.readString();  // read until timeout
-    messageToSend.trim();                 // remove any \r \n whitespace at the end of the String
-    transmissionCount= 0;
+void loop()
+{
+
+  if (Serial.available() > 0)
+  {
+    messageToSend = Serial.readString(); // read until timeout
+    messageToSend.trim();                // remove any \r \n whitespace at the end of the String
+    transmissionCount = 0;
     sendMessage(messageToSend);
   }
-
 }
--- esp-now-serial-chat/macaddr.ino
+++ esp-now-serial-chat/macaddr.ino
@@ -1,13 +1,14 @@
 // #include <ESP8266WiFi.h>
 // #include <espnow.h>
 
-// void setup(){
-//   Serial.begin(115200);
-//   Serial.println();
-//   Serial.print("ESP8266 Board MAC Address:  ");
-//   Serial.println(WiFi.macAddress());
+// void setup()
+// {
+//     Serial.begin(115200);
+//     Serial.println();
+//     Serial.print("ESP8266 Board MAC Address:  ");
+//     Serial.println(WiFi.macAddress());
 // }
 
-// void loop(){
-
+// void loop()
+// {
 // }
